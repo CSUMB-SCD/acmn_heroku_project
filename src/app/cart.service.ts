c6cc10e65f776ac06bb91c35ff7ee3dc0d6bec07
algoodwin12 <algoodwin@csumb.edu>
2018-12-06 14:37:18 -0800

    Add Cart
    One item is able to be added to the cart, checkout still not showing items

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,18 +1,36 @@
+import { Checkout } from './checkout';
+import { PhoneList } from './PhoneList';
+import { PhonesComponent } from './phones/phones.component';
+import { Phone } from './phone';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
-
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+  addItem(id: Number, arg1: number): any {
+    throw new Error("Method not implemented.");
+  }
 
+  
   constructor(private http: HttpClient, private cookieService: CookieService) { }
 
   public addToCart(id: String, qty: Number){
+
     let username = this.cookieService.get("user");
     let item = {"prod_id": id, "qty":qty};
-    let url = "https://freedomphones-cartsvc.herokuapp.com/addItem/" + username
-    return this.http.post(url, item);
+    let url = "https://freedomphones-zuul-svc.herokuapp.com/cart-service/addItem/" + username
+    return this.http.post<Phone>(url, item);
   }
+
+   getItems(){
+    let username = this.cookieService.get("user");
+    let url = "https://freedomphones-zuul-svc.herokuapp.com/cart-service/getCart/" + username
+    console.log(this.http.get(url));
+    return(this.http.get<Checkout>(url));
+
+  }
+
+
 }
--- src/app/checkout-page/checkout-page.component.ts
+++ src/app/checkout-page/checkout-page.component.ts
@@ -1,25 +1,26 @@
+import { Checkout } from './../checkout';
+import { CartService } from './../cart.service';
 import { PhoneService } from './../phone.service';
 import { Phone } from './../phone';
 import { Component, OnInit } from '@angular/core';
-
+import { PhoneList } from '../PhoneList';
 @Component({
   selector: 'app-checkout-page',
   templateUrl: './checkout-page.component.html',
   styleUrls: ['./checkout-page.component.scss']
 })
 export class CheckoutPageComponent implements OnInit {
-  phones$: Object;
+  phones$: any;
   // tslint:disable-next-line:no-shadowed-variable
-  constructor(private PhoneService: PhoneService) { }
+  constructor(private PhoneService: PhoneService, private cartService: CartService) { }
 
   ngOnInit() {
     this.getPhones();
   }
 
-  getPhones(): void {
-    this.PhoneService.getPhones().subscribe(
+  getPhones() {
+    this.cartService.getItems().subscribe(
       // tslint:disable-next-line:no-shadowed-variable
-      Phone => this.phones$ = Phone
-    );
+      Phone => this.phones$ = Phone);
    }
 }
