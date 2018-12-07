a7e9fb14cad337d1e228d718134fad0912bb5b0c
cforsythe <cforsythe@csumb.edu>
2018-12-06 16:44:31 -0800

    fixed server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + 'dist/projectPhones'));
+app.use(express.static(__dirname + '/dist/projectPhones'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + 'dist/projectPhones/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/projectPhones/index.html'));
 });
 
 // default Heroku port
