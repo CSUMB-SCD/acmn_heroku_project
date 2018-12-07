6d9502a47a5b60a49cc5c324e30af0341682eecd
algoodwin12 <algoodwin@csumb.edu>
2018-12-07 15:41:52 -0800

    Details numbers work
    The quanitty can be used within details page if user wants

--- src/app/details-page/details-page.component.ts
+++ src/app/details-page/details-page.component.ts
@@ -1,3 +1,5 @@
+import { MessageService } from './../message.service';
+import { CartService } from './../cart.service';
 import { Observable } from 'rxjs';
 import { PhoneService } from './../phone.service';
 import { Phone } from './../phone';
@@ -14,10 +16,13 @@ export class DetailsPageComponent implements OnInit {
 
   phone_id$: string;
   phone: Phone;
+  submitted = false;
+  value: number;
 
   constructor( private route: ActivatedRoute,
     // tslint:disable-next-line:no-shadowed-variable
-    private Phone: PhoneService, private location: Location) {
+    private Phone: PhoneService, private location: Location, private CartService: CartService,
+      private messageService: MessageService) {
       this.route.params.subscribe( params => this.phone_id$ = params.id);
     }
 
@@ -27,4 +32,23 @@ export class DetailsPageComponent implements OnInit {
       Phone => this.phone = Phone
     );
   }
+
+  addItemsD(value: number){
+    console.log("value" + value);
+    this.submitted = true;
+    if(this.phone.qty == 0){
+      return;
+    }
+    this.CartService.addToCart(this.phone_id$, value).subscribe(
+      result => {
+        console.log(result);
+      },
+      error => {
+      
+      },
+      () => {
+      
+      }
+    );
+  }  
 }
